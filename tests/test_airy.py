52c028c093a3fda4cb9947b3f4f08ed97c8c596b
Kenzie Stinson <kenziejstinson@gmail.com>
2026-05-09 15:56:59 -0400

    airy test

---
+++ tests/test_airy.py
@@ -0,0 +1,21 @@
+"""
+First test for the algorithm: Airy Functions
+"""
+
+## IMPORTS ##
+import numpy as np
+import pytest
+from numba import cuda
+
+from examples.airy import (make_grid,airy_coeffs,airy_cpu,airy_cuda,)
+
+@pytest.mark.skipif(not cuda.isavailable(), reason="CUDA not available")
+def test_airy():
+    N=64
+    _,_,X,Y,Z = make_grid(N,-3,-3,-3,3)
+    coeffs = airy_coeffs(80)
+
+    cpu_mag = np.abs(airy_cpu(Z,coeffs))
+    gpu_mag = airy_cuda(X,Y,coeffs)
+
+    assert np.allclose(cpu_mag,gpu_mag,rtol=1e-12,atol=1e-14)
